6a3c83e015f9a8e7628bc290af0b4228991e35be
Alexandre B <lxndr.bertrand@gmail.com>
2015-10-01 21:38:25 +0200

    Ajout de quelques TU afin de faire beuger l'appli. Mais Il en manque encore

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -15,6 +15,7 @@ namespace TU_FS
         public Directory ProMac;
         public Directory AMortJava;
         public Directory ViveBSD;
+        public Directory FreeBSD;
         public File CeciEstUneFile;
 
         [TestInitialize]
@@ -29,11 +30,15 @@ namespace TU_FS
             TDDDirectory = (Directory)TDD;
             TDDDirectory.chmod(7);
             TDDDirectory.mkdir("ViveBSD");
+            ViveBSD = (Directory)TDDDirectory.cd("ViveBSD");
+            ViveBSD.chmod(7);
+            ViveBSD.mkdir("FreeBSD");
+            FreeBSD = (Directory)ViveBSD.cd("FreeBSD");
 
             CeciEstUneFile = courants.cd("CeciEstUneFile");
             ProMac = (Directory)courants.cd("ProMac");
             AMortJava = (Directory)courants.cd("AMortJava");
-            ViveBSD = (Directory)TDDDirectory.cd("ViveBSD");
+            
 
         }
 
@@ -47,9 +52,37 @@ namespace TU_FS
         }
 
         [TestMethod]
+        public void NoFileCd()
+        {
+            File recup = courants.cd("TDDs");
+            Assert.AreEqual(recup, null);
+        }
+
+        [TestMethod]
+        public void NoRightCd()
+        {
+            TDD.Permissions = 0;
+            File recup = courants.cd("TDD");
+            Assert.AreEqual(recup, null);
+        }
+
+        [TestMethod]
         public void Mkdir()
         {
             Assert.IsTrue(courants.mkdir("Test"));            
+        }
+
+        [TestMethod]
+        public void MkdirSameName()
+        {
+            Assert.IsFalse(courants.mkdir("TDD"));
+        }
+
+        [TestMethod]
+        public void MkdirNoRight()
+        {
+            courants.Permissions = 0;
+            Assert.IsFalse(courants.mkdir("TDD"));
         }
 
         [TestMethod]
@@ -66,15 +99,69 @@ namespace TU_FS
         }
 
         [TestMethod]
+        public void GetPathRacine()
+        {
+            String recup = courants.getPath();
+            Assert.AreEqual("", recup);
+        }
+
+        [TestMethod]
+        public void GetMorePath()
+        {
+            String recup = FreeBSD.getPath();
+            Assert.AreEqual("TDD/ViveBSD/FreeBSD/", recup);
+        }
+
+        [TestMethod]
+        public void GetPathNoRight()
+        {
+            FreeBSD.Permissions = 0;
+            String recup = FreeBSD.getPath();
+            Assert.AreEqual(null, recup);
+        }
+
+        [TestMethod]
         public void Ls()
         {        
             Assert.AreEqual(courants.ls().Count, 4);
         }
 
         [TestMethod]
+        public void LsNothing()
+        {
+            Assert.AreEqual(FreeBSD.ls().Count, 0);
+        }
+
+        [TestMethod]
+        public void LsNoRight()
+        {
+            courants.Permissions = 0;
+            Assert.AreEqual(FreeBSD.ls().Count, 0);
+        }
+
+        [TestMethod]
         public void GetRoot()
         {
             Assert.AreEqual(ViveBSD.getRoot(), TDDDirectory);
+        }
+
+        [TestMethod]
+        public void GetMoreRoot()
+        {
+            Assert.AreEqual(FreeBSD.getRoot(), TDDDirectory);
+        }
+
+        [TestMethod]
+        public void GetRootSlash()
+        {
+            Assert.AreEqual(courants.getRoot(), courants);
+        }
+
+        [TestMethod]
+        public void GetRootNoRight()
+        {
+            ViveBSD.chmod(0);
+            Assert.AreEqual(ViveBSD.getRoot(), null);
         }
 
         [TestMethod]
@@ -86,6 +173,31 @@ namespace TU_FS
         }
 
         [TestMethod]
+        public void RenameExistingName()
+        {
+            AMortJava.Permissions = 7;
+            courants.rename("AMortJava", "ProMac");
+            Assert.AreEqual(AMortJava.Nom, "AMortJava");
+        }
+
+        [TestMethod]
+        public void RenameNoRightChild()
+        {
+            AMortJava.Permissions = 0;
+            courants.rename("AMortJava", "test");
+            Assert.AreEqual(AMortJava.Nom, "AMortJava");
+        }
+
+        [TestMethod]
+        public void RenameNoRightParent()
+        {
+            courants.Permissions = 0;
+            courants.rename("AMortJava", "test");
+            Assert.AreEqual(AMortJava.Nom, "AMortJava");
+        }
+
+
+        [TestMethod]
         public void CreateNewFile()
         {
             int pre = courants.contenu.Count;
@@ -93,6 +205,24 @@ namespace TU_FS
             Assert.AreEqual(pre + 1, courants.contenu.Count);
         }
 
+
+        [TestMethod]
+        public void CreateNewFileExistingName()
+        {
+            int pre = courants.contenu.Count;
+            courants.createNewFile("ProMac");
+            Assert.AreEqual(pre, courants.contenu.Count);
+        }
+
+        [TestMethod]
+        public void CreateNewFileNoRight()
+        {
+            courants.Permissions = 0;
+            int pre = courants.contenu.Count;
+            courants.createNewFile("TU");
+            Assert.AreEqual(pre, courants.contenu.Count);
+        }
+        
         [TestMethod]
         public void GetParent()
         {
@@ -103,6 +233,15 @@ namespace TU_FS
         public void Search()
         {
             Assert.AreEqual(courants.search("AMortJava").Count, 1); 
+        }
+
+        [TestMethod]
+        public void SearchMore()
+        {
+            ViveBSD.Permissions = 7;
+            FreeBSD.Permissions = 7;
+            ViveBSD.rename("FreeBSD", "AMortJava");
+            Assert.AreEqual(courants.search("AMortJava").Count, 2);
         }
 
         [TestMethod]
